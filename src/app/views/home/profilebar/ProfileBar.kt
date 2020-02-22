0c3dfa6f2f783f6f8caba8d3a094e46a9de1c535
Pedro Gomez <makhnnar@gmail.com>
2020-02-21 20:04:14 -0400

    added social networks icons

--- src/app/views/home/profilebar/ProfileBar.kt
+++ src/app/views/home/profilebar/ProfileBar.kt
@@ -6,6 +6,18 @@ import react.dom.div
 import react.dom.img
 import react.dom.p
 
+//todo: put this link on the footer <a target="_blank" href="https://icons8.com/icons/set/linkedin">LinkedIn icon</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+
+@JsModule("src/app/views/home/profilebar/icons8-linkedin-out.svg")
+external val linkedInLogo: dynamic
+
+@JsModule("src/app/views/home/profilebar/icons8-github.svg")
+external val githubLogo: dynamic
+
+@JsModule("src/app/views/home/profilebar/icons8-twitter.svg")
+external val twitterLogo: dynamic
+
+
 fun RBuilder.profileBar(
         profileData: ProfileData
 ) {
@@ -21,6 +33,21 @@ fun RBuilder.profileBar(
         }
         p {
             +profileData.description
+        }
+        img {
+            attrs{
+                src = linkedInLogo
+            }
+        }
+        img {
+            attrs{
+                src = githubLogo
+            }
+        }
+        img {
+            attrs{
+                src = twitterLogo
+            }
         }
     }
 }
